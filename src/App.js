54ccd6e60c78be229fa093ee54ef78a592079e16
Ryan Heupler <rheupler@gmail.com>
2019-09-21 13:38:04 -0400

    demo initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Class from "./components/Class";
+import Func from "./components/Func";
+import Refactored from "./components/Refactored";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Class /> */}
+        {/* <Func /> */}
+        <Refactored />
       </header>
     </div>
   );
