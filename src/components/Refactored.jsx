223964358ba82b63447d3ab9c72afd31a4f062c7
Ryan Heupler <rheupler@gmail.com>
2019-11-12 12:48:09 -0500

    refactor for use state

--- src/components/Refactored.jsx
+++ src/components/Refactored.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 const API = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function Refactored() {
+  const [name, setName] = useState("Placeholder Name");
+  const [lat, setLat] = useState("Placeholder Lat");
+  const [long, setLong] = useState("Placeholder Long");
+  const [counter, setCounter] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [hasCounter, setHasCounter] = useState(false);
   return (
     <div>
       <p>Hello, world!</p>
@@ -10,6 +16,8 @@ export default function Refactored() {
 }
 
 function Counter() {
+  const [updatedCounter, setUpdatedCounter] = useState(0);
+  const [render, setRender] = useState(0);
   return (
     <div>
       <p>Counter: counter</p>
